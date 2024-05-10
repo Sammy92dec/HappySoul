426e4ed29484154a9d816c389c200c84820068fe
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-05-10 07:14:13 +0000

    Connec html and css files.Add bootstrap link and css link

--- happytum/urls.py
+++ happytum/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("soul.urls"), name="soul-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("soul.urls"), name="soul-urls"),
+    
 ]
