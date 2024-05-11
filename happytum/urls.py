4c4403153ceb2270c20bbf7afd9faca3db85bc0f
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-05-11 04:18:20 +0000

    Add images and logo fir the page

--- happytum/urls.py
+++ happytum/urls.py
@@ -22,6 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("soul.urls"), name="soul-urls"),
-    
+    path('', include('soul.urls'), name='soul-urls'),    
 ]
